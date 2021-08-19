15bee1645d83e68748628f899664e35cd801ad97
Derek Wheat <trigostruck@gmail.com>
2021-08-19 15:20:26 -0600

    got the mons listing from pokeapi working on pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PokemonList from './PokemonList'
+import axios from 'axios'
+import Pagination from './Pagination';
 
 
 function App() {
+  const [pokemon, setPokemon] = useState([])
+  const [currentPageURL, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon/")
+  const [nextPageURL, setNextPageUrl] = useState()
+  const [prevPageURL, setPrevPageUrl] = useState()
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setLoading(true)
+    let cancel
+    axios.get(currentPageURL, {
+      cancelToken: new axios.CancelToken(c => cancel = c)
+    }).then(res => {
+      setLoading(false)
+      setNextPageUrl(res.data.next)
+      setPrevPageUrl(res.data.previous)
+      setPokemon(res.data.results.map(p => p.name))
+    })
+
+    return () => cancel()
+
+  }, [currentPageURL])
+
+  function gotoNextPage() {
+    setCurrentPageUrl(nextPageURL)
+  }
+
+  function gotoPrevPage() {
+    setCurrentPageUrl(prevPageURL)
+  }
+
+
+  if (loading) return "loading..."
+
   return (
-    null
+    <>
+    <PokemonList pokemon={pokemon} />
+    <Pagination 
+    gotoNextPage={gotoNextPage}
+    gotoPrevPage={gotoPrevPage}
+    />
+    </>
   );
 }
 
